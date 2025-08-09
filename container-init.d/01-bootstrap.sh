978cec224371219999c428c8f6beec1342a41e81
Ed Summers <ehs@pobox.com>
2025-08-09 11:18:36 -0400

    docs

--- container-init.d/01-bootstrap.sh
+++ container-init.d/01-bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -ex
 ipfs bootstrap rm all
-#ipfs bootstrap add "/ip4/$PRIVATE_PEER_IP_ADDR/tcp/4001/ipfs/$PRIVATE_PEER_ID"
+# uncomment on non-bootstrap nodes to tell ipfs where to find other peers
+# ipfs bootstrap add "/ip4/$PRIVATE_PEER_IP_ADDR/tcp/4001/ipfs/$PRIVATE_PEER_ID"
