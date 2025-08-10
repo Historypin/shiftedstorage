ce738d3c59b6d54da92131b5b8d33461dc8b450d
Ed Summers <ehs@pobox.com>
2025-08-10 15:12:54 -0400

    Announce Tailscale IP

--- container-init.d/01-ipfs-bootstrap.sh
+++ container-init.d/01-ipfs-bootstrap.sh
@@ -9,4 +9,7 @@ if [[ -n "${IPFS_BOOTSTRAP}" ]]; then
     ipfs bootstrap add "${IPFS_BOOTSTRAP}"
 fi
 
+TAILSCALE_IP=`ip -f inet addr show tailscale0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p'`
+
 ipfs config --json Addresses.Swarm '["/ip4/0.0.0.0/tcp/4001", "/ip6/::/tcp/4001"]'
+ipfs config --json Addresses.Announce [\"/ip4/${TAILSCALE_IP}/tcp/4001\"]
